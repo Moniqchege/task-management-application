117b0f0819f57966f7e2eed67e177978f435d60e
MoniqChege <quichege33@gmail.com>
2025-04-28 11:36:33 +0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/task-list',
+        pathMatch: 'full'
+    },
+    {
+        path: 'task-list',
+        component: TaskListComponent
+    },
+    {
+        path: 'task-form',
+        component: TaskFormComponent
+    },
+    {
+        path: 'notifications',
+        component: NotificationsComponent
+    }
+];
